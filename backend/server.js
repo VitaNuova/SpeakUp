917f53b464272b11f92fdbec2fc8298d0f0f8506
Branislav Vidojevic <branislav.vidojevic@msg-global.com>
2017-06-13 11:42:25 +0200

    Implements backend and frontend application as shown in the lectures

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 var config = require('./config/config');
 var app = require('./app');
 
-
-
+/**
+ * Start the server
+ */
+app.listen(config.app.port, function() {
+    console.log('App listening on port 3000!');
+});
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,7 +1,43 @@
 'use strict';
 
-let app = angular.module('app', []);
+import angular from 'angular';
+import uiRouter from '@uirouter/angularjs';
 
+import angularMaterial from 'angular-material';
+import 'angular-material/angular-material.css';
+
+import ngMdIcons from 'angular-material-icons';
+
+import EventsService from './services/events/events';
+import UserService from './services/user/user';
+
+import Routes from './config/routes';
+import Middlewares from './config/middlewares';
+
+import AppContent from './components/app-content/app-content';
+import ViewEvents from './components/view-events/view-events';
+import ViewEvent from './components/view-event/view-event';
+import ViewEventEdit from './components/view-event-edit/view-event-edit';
+import ViewEventCreate from './components/view-event-create/view-event-create';
+import ViewLogin from './components/view-login/view-login';
+
+let app = angular.module('app', [
+    uiRouter,
+    angularMaterial,
+    ngMdIcons,
+    UserService.name,
+    EventsService.name,
+    AppContent.name,
+    ViewEvents.name,
+    ViewEvent.name,
+    ViewEventEdit.name,
+    ViewEventCreate.name,
+    ViewLogin.name
+]);
+
+app.constant('API_URL', 'http://localhost:3000/api');
+app.config(Routes);
+app.config(Middlewares);
 
 
 angular.element(document).ready(function() {
