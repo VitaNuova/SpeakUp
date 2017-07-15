9fdc72f1fefcfc7577f908135b58c730e6d4a05a
Marija Jovanovic <ga83cay@mytum.de>
2017-07-15 15:57:44 +0200

    Initial commit

--- frontend/src/components/view-profile/view-profile.component.js
+++ frontend/src/components/view-profile/view-profile.component.js
@@ -3,35 +3,82 @@
 import template from './view-profile.template.html';
 import EventsService from './../../services/events/events.service';
 import UserService from './../../services/user/user.service';
+import TopicsService from './../../services/topics/topics.service';
+import LanguagesService from './../../services/languages/languages.service';
+import LanguageLevelsService from './../../services/language-levels/language-levels.service';
+
+import './view-profile.style.css';
 
 class ViewProfileComponent {
-    constructor() {
-        this.controller = ViewProfileComponentController;
-        this.template = template;
-        this.bindings = {
-            event: '<',
-        }
-
+  constructor() {
+    this.controller = ViewProfileComponentController;
+    this.template = template;
+    this.bindings = {
+      user: '<',
     }
 
-    static get name() {
-        return 'viewProfile';
-    }
+  }
+
+  static get name() {
+    return 'viewProfile';
+  }
 
 
 }
 
 class ViewProfileComponentController {
-    constructor($state, EventsService, UserService) {
-        this.$state = $state;
-        this.EventsService = EventsService;
-        this.UserService = UserService;
+  constructor($state, EventsService, UserService, TopicsService, LanguagesService, LanguageLevelsService) {
+    this.$state = $state;
+    this.EventsService = EventsService;
+    this.UserService = UserService;
+    this.TopicsService = TopicsService;
+    this.LanguagesService = LanguagesService;
+    this.LanguageLevelsService = LanguageLevelsService;
 
-    }
+    this.getLanguages();
+    this.getTopics();
+    this.getLevels();
+  }
 
-    static get $inject() {
-        return ['$state', EventsService.name, UserService.name];
+  getCurrentUser(){
+    return this.UserService.getCurrentUser();
+  }
+
+  getUserLanguages() {
+    return this.UserService.getCurrentUser().languages;
+  }
+
+  getLanguages() {
+    this.LanguagesService.list().then(response => {
+      this.languages = response;
+    });
+  }
+
+  getLevels() {
+    this.LanguageLevelsService.list().then(response => {
+      this.levels = response;
+    });
+  }
+
+  getTopics() {
+    if (!this.topics) {
+      this.TopicsService.list().then(response => {
+        this.topics = response;
+      });
     }
+  }
+
+  addNewLanguage() {
+
+  }
+
+  addNewTopic() {
+
+  }
+
+  static get $inject() {
+    return ['$state', EventsService.name, UserService.name, TopicsService.name, LanguagesService.name, LanguageLevelsService.name];
+  }
 
 }
 
